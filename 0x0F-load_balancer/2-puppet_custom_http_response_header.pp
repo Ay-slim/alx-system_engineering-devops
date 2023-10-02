e624a93d1958e74b9cd9b23c8fb2077e353b679e
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-02 14:13:34 -0700

    Add header directly

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,17 +9,7 @@ exec {'install':
   provider => shell,
 }
 
-exec {'Hello World!':
-  command  => 'echo "Hello World!" > /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'Replace sites available':
-  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;\\n\\t}/" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-
-exec {'Replace sites available':
+exec {'Include header':
   command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
   provider => shell,
 }
